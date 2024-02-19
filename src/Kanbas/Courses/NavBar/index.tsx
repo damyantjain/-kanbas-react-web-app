7d80745f9229634dfb54c2e6d29832b1d83b9976
damyant <damyant9999@gmail.com>
2024-02-19 17:49:03 -0500

    Add index.css to NavBar component

--- src/Kanbas/Courses/NavBar/index.tsx
+++ src/Kanbas/Courses/NavBar/index.tsx
@@ -1,6 +1,7 @@
 import { courses } from "../../../Kanbas/Database";
 import { Link, useLocation, useParams } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
+import "./index.css";
 import StudentViewButton from "../StudentViewButton";
 
 function Navbar() {
@@ -16,7 +17,7 @@ function Navbar() {
                     <div className="col">
                         <div className="d-flex">
                             <Link to={"#"}><FaBars className="me-2" style={{ marginBottom: "3px", color: "#ff7070" }} /></Link>
-                            <nav className="wd-flex-grow-1 wd-inline" aria-label="breadcrumb">
+                            <nav className="wd-flex-grow-1 wd-inline wd-nav-style" aria-label="breadcrumb">
                                 <ol className="breadcrumb">
                                     <li className="breadcrumb-item wd-top-bar"><Link to={`/Kanbas/Courses/${course?._id}/Home`}>{course?.number}</Link></li>
                                     <li className="breadcrumb-item active wd-top-bar-text"><Link to={"#"}>{decodeURIComponent(screen)}</Link></li>
