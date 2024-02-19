644dd646d52c65531b5ff10eb699115d7b9d1bcc
damyant <damyant9999@gmail.com>
2024-02-19 13:40:21 -0500

    Lab changes implemented

--- src/Labs/a3/JavaScript/WorkingWithFunctions.tsx
+++ src/Labs/a3/JavaScript/WorkingWithFunctions.tsx
@@ -1,7 +1,16 @@
-function WorkingWithFunctions(){
-    return(
+import WorkingWithArrays from "./arrays/WorkingWithArrays";
+import ArrowFunction from "./functions/ArrowFunctions";
+import LegacyES5Function from "./functions/ES5Functions";
+import FunctionParanthesisAndParameters from "./functions/FunctionParenthesisAndParameters";
+import ImpliedReturn from "./functions/ImpliedReturn";
+function WorkingWithFunctions() {
+    return (
         <div>
-            
+            <LegacyES5Function />
+            <ArrowFunction />
+            <ImpliedReturn />
+            <FunctionParanthesisAndParameters />
+            <WorkingWithArrays />
         </div>
     );
 }
